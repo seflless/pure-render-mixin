f7a83a3d84ead0535849a2fbf93e188651434cec
Francois Laberge <francois@jibo.com>
2015-05-10 13:17:15 -0400

    Some simple refactoring

--- tests/pure-render-mixin.js
+++ tests/pure-render-mixin.js
@@ -3,16 +3,16 @@ var assert = require('assert'),
     pureRenderMixin = require('../src/index.js').PureRenderMixin;
 
 describe('PureRenderMixin', function(){
-    it('should prevent render when \'this.props\' hasn\'t change', function(){
+    it('should prevent render when \'this.props\' hasn\'t changed', function(){
 
     })
-    it('should prevent render when \'this.state\' hasn\'t change', function(){
+    it('should prevent render when \'this.state\' hasn\'t changed', function(){
 
     })
-    it('should render when \'this.props\' don\'t change', function(){
+    it('should render when \'this.props\' has changed', function(){
 
     })
-    it('should render when \'this.stats\' don\'t change', function(){
+    it('should render when \'this.stats\' has changed', function(){
 
     })
 })
