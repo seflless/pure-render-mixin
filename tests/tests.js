08b6dd3115fea0ed587042659ca6157a8513481d
Francois Laberge <francois@jibo.com>
2015-05-10 13:03:47 -0400

    Ported over comments from Facebook

--- tests/tests.js
+++ tests/tests.js
@@ -1,13 +1,14 @@
 // TODO:
-/*
-var assert = require("assert"),
+
+var assert = require('assert'),
+    react = require('react')
     pureRenderMixin = require('../src/index.js').PureRenderMixin,
     shallowCompare = require('../src/index.js').shallowCompare,
     shallowEqual = require('../src/index.js').shallowEqual;
-
+/*
 describe('PureRenderMixin', function(){
     it('should prevent render', function(){
-      
+
     })
 })
 */
